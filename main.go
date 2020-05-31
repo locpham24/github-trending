1af0b456d3a97bf37ded532939f469b7921f3ed7
locpham24 <loc.pham2412@gmail.com>
2020-05-31 21:41:50 +0700

    use os port

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/locpham24/github-trending/handler"
+	"log"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	e := echo.New()
 
 	handler.InitRouter(e)
-	e.Logger.Fatal(e.Start(":7000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
